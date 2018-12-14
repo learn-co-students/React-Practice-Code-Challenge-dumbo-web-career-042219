548c7aa36b1b458713176d6b880a7e773b10ac71
Lane Miller <35706746+LaneMiller@users.noreply.github.com>
2018-12-14 15:09:45 -0500

    Update SushiContainer.js

--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -4,19 +4,18 @@ import MoreButton from '../components/MoreButton'
 
 const SushiContainer = (props) => {
 	return (
-		<Fragment>
-			<div className="belt">
-				{
-					props.sushis.map((sushi) => {
-						return <Sushi sushi={sushi}
-													key={sushi.id}
-													eat={props.eat}
-													taken={props.eaten.includes(sushi)}/>
-					})
-				}
-				<MoreButton more={props.more}/>
-			</div>
-		</Fragment>
+	  <Fragment>
+	    <div className="belt">
+	      { props.sushis.map((sushi) => {
+		return <Sushi sushi={sushi}
+			      key={sushi.id}
+			      eat={props.eat}
+			      taken={props.eaten.includes(sushi)}/>
+		})
+	      }
+	      <MoreButton more={props.more}/>
+	    </div>
+	 </Fragment>
 	)
 }
 
